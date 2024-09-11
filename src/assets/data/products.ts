b345c85a7dc87be2bb940094adbf8fa88db9d29e
Roluna1 <christiandelroluna19@gmail.com>
2024-09-12 06:58:51 +0800

    Uploading for phone

--- src/assets/data/products.ts
+++ src/assets/data/products.ts
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, get } from "firebase/database";
-
 export interface FormatProduct {
   id: string;
   img: string;
